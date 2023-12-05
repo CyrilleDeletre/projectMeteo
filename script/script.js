09591d34718947fe128575e7cab34150f9792aac
CyrilleDeletre <cyrille.deletre@gmail.com>
2023-12-05 09:37:41 +0100

    push github

--- script/script.js
+++ script/script.js
@@ -1,39 +1,52 @@
-
+// Fonction générique pour créer des éléments html dynamiquement
 function createElementHtml(type, parent, id, className, contenu, event) {
+    // Permet de créer un élément html en fonction du paramètre type
     let elementHtml = document.createElement(type);
+    // Permet d'ajouter cet élément html à un parent en fonction du paramètre parent
     parent.appendChild(elementHtml);
-
+    // Permet d'ajouter à cet élément html une id et une classe
     elementHtml.id = id;
     elementHtml.className = className;
 
-    //si on a une balise text : on creer du texte
-    if (true){
-        elementHtml.textContent = contenu;
+    // Permet d'ajouter du contenu à cet élément html en fonction du paramètre contenu
+    // Si on a crée un élément html de texte : on change le contenu du texte avec le paramètre contenu
+    if (type === 'p' || type === 'h2' || type === 'button'){
+        elementHtml.textContent = contenu
     }
-    // // sinon, si c'est une balise image : 
-    // // on créer une source et on donne un chemin a cette source
-    // else if(si on est dans une img = true){
-    //     elementHtml.src = contenu
-    // }
-    
+     
+    // // Si on a crée un élément html d'image : on change ajoute une source d'image avec le paramètre contenu
 
+    if (type === 'img'){
+        elementHtml.src = contenu
+    }
     
-    
+    // // Si le paramètre event est présent dans l'élément html, et si il correspond bien à une fonction :
     if (event && typeof event === 'function') {
+        // Ajouter un écouteur d'évènement
         elementHtml.addEventListener('click', event);
     }
+    // On retourne la variable elementHTML afin de pouvoir l'utiliser en dehors de la fonction
     return elementHtml;
 }
 
-function afficheMeteo(ville) {
+// Fonction pour afficher ou non les informations supplémentaires au clique d'une ville
+function afficheInfosMeteo(ville) {
+    // On créer une variable pour stocker un conteneur pour chaque infos par ville
     let divGlobal = document.getElementById(`divGlobal${ville.nom}`);
+    // On lui ajoute le style css display none par défaut et un toggle pour l'affiche ou non au clique
     divGlobal.style.display = (divGlobal.style.display === 'none') ? 'flex' : 'none';
 }
 
+// Fetch du json si l'api ne marche pas
 fetch('../script/apiMeteo.json')
+
+// Fetch de l'api
+// fetch('http://57.129.5.9:3000/villes')
     .then(response => response.json())
     .then(data => {
+        // On fetch les données de l'api que l'on stocke dans data et on cherche pour chaque ville
         data.forEach(ville => {
+            // On créer des div pour afficher les informations
             let divInfos
             let divOtherInfos
             let divGlobal = createElementHtml('div', document.body, `divGlobal${ville.nom}`, 'div-global');
@@ -44,12 +57,12 @@ fetch('../script/apiMeteo.json')
 
             // Div pour afficher les informations (logo / temperature / autre infos)
             divInfos = createElementHtml('div', divGlobal, `divInfos${ville.nom}`, 'div-infos');
-            // createElementHtml('img', divInfos, `conditionsMeteo${ville.nom}`, `conditions-meteo-${ville.nom.toLowerCase()}` `src de l'image`);
+            createElementHtml('img', divInfos, `conditionsMeteo${ville.nom}`, `conditions-meteo-${ville.nom.toLowerCase()}`, `/img/${ville.conditionsMeteo}.jpg`);
             createElementHtml('p', divInfos, `temperature${ville.nom}`, `temperature-${ville.nom.toLowerCase()}`, `${ville.temperature}°C`);
 
             // Div pour afficher les autres infos
             divOtherInfos = createElementHtml('div', divInfos, `divOtherInfos${ville.nom}`, 'div-other-infos');
-            createElementHtml('button', document.body, `myButton${ville.nom}`, 'my-button', ville.nom, () => afficheMeteo(ville));
+            createElementHtml('button', document.body, `myButton${ville.nom}`, 'my-button', ville.nom, () => afficheInfosMeteo(ville));
             createElementHtml('p', divOtherInfos, `humidite${ville.nom}`, `humidite-${ville.nom.toLowerCase()}`, `humidité : ${ville.humidite}`);
             createElementHtml('p', divOtherInfos, `precipitations${ville.nom}`, `precipitations-${ville.nom.toLowerCase()}`, `précipitations : ${ville.precipitations}`);
             createElementHtml('p', divOtherInfos, `indiceUV${ville.nom}`, `indice-uv-${ville.nom.toLowerCase()}`, `indice UV : ${ville.indiceUV}`);
